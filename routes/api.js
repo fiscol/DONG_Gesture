554af2dab28ae59327f242b5ea9cbbe75c93d070
BigQ <daviddrmflstd@yahoo.com.tw>
2016-08-03 20:53:18 +0800

    2016/08/03

--- routes/api.js
+++ routes/api.js
@@ -70,6 +70,7 @@ DB Part
 var localurl;
 router.post('/localurl', function (req, res, next){
     localurl = req.body.url;
+    res.send(localurl);
 });
 // 傳到DongMotion測試
 function _requestDongSlide(){
