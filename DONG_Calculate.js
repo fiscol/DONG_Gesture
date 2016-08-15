7fb72549b9439c8dfd8538b42d563c1a47dec78b
fiscol <haopiongwoops@gmail.com>
2016-08-15 17:13:53 +0800

    20160815-Add RawData Response on View + DB-Fiscol

--- DONG_Calculate.js
+++ DONG_Calculate.js
@@ -1,5 +1,9 @@
 exports._getDateTimeNow = function(){
- // Time Log Test
+    return _getDateTimeNow();
+}
+
+var _getDateTimeNow = function(){
+    // Time Log Test
   var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth()+1; //January is 0!
@@ -31,28 +35,19 @@ exports._getDateTimeNow = function(){
 }
 
 exports._postData = function(data){
- 
-
-
   var RandomSpeed = (Math.floor((Math.random() * 10) + 1))*17;
   var RandomPower = (Math.floor((Math.random() * 10) + 1))*37;
-  var RandomSimilarity = (Math.floor((Math.random() * 10) + 1))*10;
-  var RandomGestureNum = (Math.floor((Math.random() * 3) + 1));
-  
+  var RandomGestureNum = (Math.floor((Math.random() * 3) + 1)); 
   var DateTimeNow = _getDateTimeNow();
 
   data = {
-      User: data.User,
-      RawCode: data.RawCode,
-      MotionCode: "[0]",
+      RawCode: data.ProcessCode.toString(),
+      MotionCode: data.MotionCode,
       Time: DateTimeNow,
       MaxSpeed: RandomSpeed,
       MaxPower: RandomPower,
-      Similarity: RandomSimilarity,
+      Similarity: data.Similarity,
       GestureNum: RandomGestureNum,
     }
-
-
-
-  return data
+  return data;
 }
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
+//20160815 Ver.1 Fiscol
+//Note : iOS串上RawData做ViewData串接與寫入DB測試;
+
 /*
 api.js
 */
 var express = require('express');
 var router = express.Router();
-
+var dataCounter = 0;
 /*
 API Server
 */
@@ -11,23 +14,32 @@ router.post('/iOS', function (req, res, next){
     // 解析body
     var DataRaw = req.body;
 /*
+Kernal Part
+*/
+    // 編碼處理 + 運算Rate, Pattern
+    var minderBetaService = require('../services/unit/kernal/minderbeta.js');
+    var processBetaService = require('../services/unit/kernal/processbeta.js');
+    var Threshold = 0.18;
+    // 轉換為一列編碼
+    var ProcessedCode = processBetaService._processData(DataRaw, Threshold).mixBinaryCodes;
+    var MinderThreshold = 0.5;
+    var PatternModel = 1;
+    var PatternType = 1;
+    // 運算Rate, Pattern 
+    var MinderResult = minderBetaService._lcsRateComputing(
+ProcessedCode, MinderThreshold, PatternModel, PatternType);
+/*
 Unit Part
 */
-    // minderBetaService
-    var minderBetaService = require('../services/unit/kernal/minderbeta.js');
-    // var SDKRawCode = [4, 5, 3, 3, 7, 5, 4, 4, 6, 1, 2, 2, 3, 2, 4, 4, 3, 3, 4, 4, 5, 2, 3, 1, 4, 4, 2, 3, 1, 4, 4, 2, 3, 3, 4, 4, 2, 3, 4, 6, 2];
-    var SDKThreshold = 0.55;
-    var SDKSimilarity = 1;
-    var SDKParam = 1;
-    // DataRaw.SDKRawCode is string, must to turn into arr. 
-    var string2arr = JSON.parse(DataRaw.SDKRawCode);
-    // console.log(string2arr);
-
-    var MinderResult = minderBetaService._lcsRateComputing(string2arr, SDKThreshold, SDKSimilarity, SDKParam);
-
-    //DONG_Calculate
     var api = require('../DONG_Calculate.js');
-    var DataFinish = api._postData(DataRaw);
+    var UID = DataRaw.UID;
+    var DataResult = {
+        User : UID,
+        ProcessCode : ProcessedCode,
+        MotionCode : MinderResult.ActionCode,
+        Similarity : parseInt(MinderResult.Rate * 100)
+    }
+    var DataFinish = api._postData(DataResult);
 
     // Send RealTimeData to View 
     req.io.sockets.emit('RealTimeData', { 
@@ -46,33 +58,29 @@ DB Part
 */
     var DB = require('../libraries/firebase_db.js');
     // DB Path
-    var RefPath = "DONGCloud/Test";
+    var RefPath = "DONGCloud/MotionData/" + UID;
     // Child Name
-    var RandomChildName = (Math.floor((Math.random() * 3) + 1));//Random Test
-    var ChildName = "User" + RandomChildName;
+    dataCounter++;
+    var ChildName = "Data" + dataCounter;
     // Read DB data
     // 存到DB
-    // DB._set(RefPath, ChildName, DataFinish);
+    DB._set(RefPath, ChildName, DataFinish);
 
-    DB._onValue(RefPath, ChildName, function(onValueResult){
-
-        // Send DBdata to View
-        req.io.sockets.emit('DBData', { 
-            Name: onValueResult.User,
-            // Rawdata: DataRaw.SDKRawCode,
-            Rawdata: string2arr,
-            Rate: MinderResult.Rate,
-            ActionCode: MinderResult.ActionCode
-        });
-
-        // 過門檻值則觸發DONG Motion
-        if ( MinderResult.Rate > 0.5) {
-            if (MinderResult.ActionCode == 19) {
-                _requestDongMotion();
-                console.log('Good');
-            };
-        };
+    // Send DBdata to View
+    req.io.sockets.emit('DBData', { 
+        Name: UID,
+        Rawdata: ProcessedCode,
+        Rate: MinderResult.Rate,
+        ActionCode: MinderResult.ActionCode
     });
+
+    // 過門檻值則觸發DONG Motion
+    if (MinderResult.Rate >= MinderThreshold) {
+        if (MinderResult.ActionCode == 19) {
+            _requestDongMotion();
+            console.log('Good');
+        };
+    };
     // 傳到DongSlide測試
     // _requestDongSlide();
     // 傳到DongMotion測試
